fffae0c31666a93031f5347d207b93f085fcac88
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-05-18 19:20:15 -0300

    Requisito 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, 
+{ $addToSet: { ingredientes: "bacon" } });
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
